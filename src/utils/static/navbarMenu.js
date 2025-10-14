e74d27b299f671a975ef94587f6511fbb7111221
david juniawan <david.juniawan@brank.as>
2025-10-14 07:55:06 +0700

    add doctors page

--- src/utils/static/navbarMenu.js
+++ src/utils/static/navbarMenu.js
@@ -1,7 +1,7 @@
 const navbarMenu = [
   { name: "About", path: "#", isButton: false },
   { name: "Treatment", path: "/our-treatments", isButton: false },
-  { name: "Doctors", path: "#", isButton: false },
+  { name: "Doctors", path: "/doctors", isButton: false },
   { name: "Gallery", path: "#", isButton: false },
   { name: "Promo", path: "/promo", isButton: false },
   { name: "Blog", path: "/blog", isButton: false },
